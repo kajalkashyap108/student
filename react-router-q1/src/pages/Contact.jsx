8c975de07bde30499204a5bd7d194c94e8223aa0
kajalkashyap108 <kashyapkashyap2212@gmail.com>
2025-01-30 00:03:39 +0530

    react-router-q1solved

---
+++ react-router-q1/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+    return (
+      <div>
+        <h1>Welcome to Contact Page</h1>
+        <p>This is the contact page of the application.</p>
+      </div>
+    );
+  }
+  
+  export default Contact;
---
+++ react-router-q1/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div>
+        <h1>Welcome to Home Page</h1>
+        <p>This is the home page of the application.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
